15c79fc7700a903d2d7dbd62f67fa28bbc90fc61
Jakub <jakub.sobon2@outlook.com>
2026-02-16 18:48:04 +0100

    inital commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
 
-# Sprawdźmy, czy pobieranie się udało (kod 200 oznacza sukces)
-if page.status_code == 200:
-    print("Strona pobrana pomyślnie!")
-else:
-    print(f"Błąd: {page.status_code}")
+def extract():
+    headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36'
+    URL = "https://realpython.github.io/fake-jobs/"
+    page = requests.get(URL, headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+def transform(soup):
+    divs = soup.find_all('div', class_ = 'card')
+    for item in divs:
+        title = item.find(class_ = 'title').text
+        print (title)
+    return 
+
+c = extract()
+transform(c)
